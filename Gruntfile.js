608427ae57aca62822484482e73daf3fd99c7ec2
John Lewis <john@pointful.net>
2013-12-03 21:25:04 -0700

    initial checkin of intro to docker preso

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,7 @@ module.exports = function(grunt) {
 		sass: {
 			main: {
 				files: {
+					'css/theme/docker.css': 'css/theme/source/docker.scss',
 					'css/theme/default.css': 'css/theme/source/default.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
 					'css/theme/night.css': 'css/theme/source/night.scss',
